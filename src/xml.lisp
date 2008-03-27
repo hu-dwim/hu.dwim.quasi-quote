4b55e07368a71a0ce8e34be51b10ae24b7be38b2
attila.lendvai <attila.lendvai@gmail.com>
2008-03-27 22:42:10 +0000

    smarter xml reader

--- src/xml.lisp
+++ src/xml.lisp
@@ -9,57 +9,102 @@
 ;;;;;;;;;
 ;;; Parse
 
-(define-syntax (quasi-quoted-xml :readtime-wrapper-result-transformer (lambda (result)
-                                                                        (if (rest result)
-                                                                            (make-instance 'xml-quasi-quote :body (mapcar 'body-of result))
-                                                                            (first result))))
-    (&key (quasi-quote-character #\<)
-          (quasi-quote-end-character #\>)
+(def special-variable *xml-quasi-quote-level*)
+
+(define-syntax (quasi-quoted-xml :readtime-wrapper-result-transformer
+                                 (lambda (result)
+                                   (if (rest result)
+                                       (make-instance 'xml-quasi-quote :body (mapcar 'body-of result))
+                                       (first result))))
+    (&key (open-bracket-character #\<)
+          (close-bracket-character #\>)
           (unquote-character #\,)
           (splice-character #\@))
-  (set-quasi-quote-syntax-in-readtable
-   (lambda (body)
-     (make-instance 'xml-quasi-quote
-                    :body (labels ((process (form)
-                                     (macrolet ((unless-unquote (value &body forms)
-                                                  (once-only (value)
-                                                    `(if (typep ,value 'xml-unquote)
-                                                         ,value
-                                                         (progn
-                                                           ,@forms)))))
-                                       (etypecase form
-                                         (string
-                                          (make-instance 'xml-text :content form))
-                                         (cons
-                                          (bind (((name &optional attributes &rest children) form))
-                                            (make-instance 'xml-element
-                                                           :name (name-as-string name)
-                                                           :attributes (unless-unquote attributes
-                                                                                       (iter (generate element :in attributes)
-                                                                                             (for name = (next element))
-                                                                                             (if (typep name 'xml-unquote)
-                                                                                                 (collect name)
-                                                                                                 (bind ((value (next element)))
-                                                                                                   (collect (make-instance 'xml-attribute
-                                                                                                                           :name (unless-unquote name (name-as-string name))
-                                                                                                                           :value (unless-unquote value (princ-to-string value))))))))
-                                                           :children (mapcar #'process children))))
-                                         (xml-unquote form)))))
-                            (process body))))
-   (lambda (form spliced) (make-instance 'xml-unquote :form form :spliced spliced))
-   :quasi-quote-character quasi-quote-character
-   :quasi-quote-end-character quasi-quote-end-character
-   :unquote-character unquote-character
-   :splice-character splice-character
-   :quasi-quote-reader-wrapper
-   (lambda (original-reader)
-     (lambda (stream char)
-       (bind ((*readtable* (copy-readtable)))
-         (set-macro-character quasi-quote-character
-                              (lambda (stream char)
-                                (declare (ignore char))
-                                (read-delimited-list #\> stream t)))
-         (funcall original-reader stream char))))))
+  (bind ((original-reader-on-open-bracket-character (multiple-value-list (get-macro-character open-bracket-character *readtable*)))
+         (original-reader-on-unquote-character      (multiple-value-list (get-macro-character unquote-character *readtable*))))
+    (set-macro-character open-bracket-character
+                         (make-quasi-quoted-xml-reader original-reader-on-open-bracket-character
+                                                       original-reader-on-unquote-character
+                                                       open-bracket-character close-bracket-character
+                                                       unquote-character
+                                                       splice-character)
+                         t
+                         *readtable*)))
+
+(def function make-quasi-quoted-xml-reader (original-reader-on-open-bracket-character
+                                            original-reader-on-unquote-character
+                                            open-bracket-character close-bracket-character
+                                            unquote-character splice-character)
+  (declare (ignore original-reader-on-open-bracket-character))
+  (labels ((unquote-reader (stream char)
+             (declare (ignore char))
+             (bind ((*readtable* (copy-readtable))
+                    (*xml-quasi-quote-level* (1- *xml-quasi-quote-level*))
+                    (spliced? (eq (peek-char nil stream t nil t) splice-character)))
+               (when spliced?
+                 (read-char stream t nil t))
+               (assert (<= 0 *xml-quasi-quote-level*))
+               (when (zerop *xml-quasi-quote-level*)
+                 ;; restore the original unquote reader when we are leaving our nesting. this way it's possible
+                 ;; to use #\, in its normal meanings when being outside our own nesting levels.
+                 (apply 'set-macro-character unquote-character original-reader-on-unquote-character))
+               (bind ((body (read stream t nil t)))
+                 (make-instance 'xml-unquote :form body :spliced spliced?))))
+           (toplevel-quasi-quoted-xml-reader (stream char)
+             (declare (ignore char))
+             (bind ((*xml-quasi-quote-level* (if (boundp '*xml-quasi-quote-level*)
+                                                 (1+ *xml-quasi-quote-level*)
+                                                 1))
+                    (*readtable* (copy-readtable)))
+               (set-macro-character unquote-character #'unquote-reader)
+               ;; on nested invocations we want to do something else then on the toplevel invocation
+               (set-macro-character open-bracket-character #'nested-quasi-quoted-xml-reader)
+               (bind ((next-char (peek-char nil stream t :eof t))
+                      (body (if (or (alphanumericp next-char)
+                                    (char= unquote-character next-char))
+                                (progn
+                                  ;; we must set the syntax on the end char to be like #\)
+                                  ;; until we read out our entire body. this is needed to
+                                  ;; make "<... 5> style inputs work where '5' is not
+                                  ;; separated from '>'.
+                                  (bind ((*readtable* (copy-readtable)))
+                                    (set-syntax-from-char close-bracket-character #\) *readtable*)
+                                    (read-delimited-list close-bracket-character stream t)))
+                                ;; seems like we have a standalone #\<, read it as the common-lisp:< symbol
+                                (return-from toplevel-quasi-quoted-xml-reader 'common-lisp:<))))
+                 (make-instance 'xml-quasi-quote :body (process-xml-reader-body body)))))
+           (nested-quasi-quoted-xml-reader (stream char)
+             (declare (ignore char))
+             (process-xml-reader-body (read-delimited-list close-bracket-character stream t))))
+    #'toplevel-quasi-quoted-xml-reader))
+
+(def function process-xml-reader-body (form)
+  (bind ((name (pop form))
+         (attributes (pop form)))
+    (unless name
+      (simple-reader-error "Syntax error in XML syntax, node name is NIL!?"))
+    (macrolet ((unless-unquote (value &body forms)
+                 (once-only (value)
+                   `(if (typep ,value 'xml-unquote)
+                        ,value
+                        (progn
+                          ,@forms)))))
+      (unless-unquote attributes
+        (when (typep attributes 'xml-syntax-node)
+          (push attributes form)
+          (setf attributes nil)))
+      (make-instance 'xml-element
+                     :name (name-as-string name)
+                     :attributes (unless-unquote attributes
+                                   (iter (generate element :in attributes)
+                                         (for name = (next element))
+                                         (if (typep name 'xml-unquote)
+                                             (collect name)
+                                             (bind ((value (next element)))
+                                               (collect (make-instance 'xml-attribute
+                                                                       :name (unless-unquote name (name-as-string name))
+                                                                       :value (unless-unquote value (princ-to-string value))))))))
+                     :children form))))
 
 (def (function e) with-quasi-quoted-xml-to-binary-syntax (&key stream)
   (with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'quasi-quoted-string 'quasi-quoted-binary
@@ -96,6 +141,9 @@
 
 (def (class* e) xml-text (xml-syntax-node)
   ((content)))
+
+(def (function e) make-xml-attribute (name value)
+  (make-instance 'xml-attribute :name name :value value))
 
 ;;;;;;;;;;;;;
 ;;; Transform
