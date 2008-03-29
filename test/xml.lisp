f68178a49bfb8fac6508a08ae1841847754e7f5e
attila.lendvai <attila.lendvai@gmail.com>
2008-03-29 14:16:29 +0000

    more xml tests

--- test/xml.lisp
+++ test/xml.lisp
@@ -27,16 +27,32 @@
      <element>
      <element>}))
 
+(def string=-test test/xml/element-unquoting ()
+  ("<element/>"
+   {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
+    <,"element">})
+  ("<element><nested/></element>"
+   {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
+    <element ()
+      ,(make-xml-element "nested")>})
+  ("<element><child1/><child2/><child3/><child4 attribute1=\"1\"/><child5/></element>"
+   {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
+    <element ()
+      <child1>
+      ,(make-xml-element "child2")
+      ,@(list (make-xml-element "child3")
+              (make-xml-element "child4" (list (make-xml-attribute "attribute1" "1"))))
+      <child5>>}))
+
 (def string=-test test/xml/attribute-unquoting ()
   ("<element attribute=\"1\"/>"
    {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
     <element ,(list (make-xml-attribute "attribute" "1"))>})
-  (with-expected-failures
-    ("<element attribute1=\"1\" attribute2=\"2\" attribute3=\"3\" attribute4=\"4\" aTTriUte5=\"5\" attribute6=\"6\"/>"
-     {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
-      <element (attribute1 1
-                ,(make-xml-attribute "attribute2" "2")
-                ,@(list (make-xml-attribute "attribute3" "3")
-                        (make-xml-attribute "attribute4" "4"))
-                "aTTriUte5" "5"
-                ,(make-xml-attribute "attribute6" "6"))>})))
+  ("<element attribute1=\"1\" attribute2=\"2\" attribute3=\"3\" attribute4=\"4\" aTTriUte5=\"5\" attribute6=\"6\"/>"
+   {(with-transformed-quasi-quoted-syntax 'quasi-quoted-xml 'string-emitting-form)
+    <element (attribute1 1
+                         ,(make-xml-attribute "attribute2" "2")
+                         ,@(list (make-xml-attribute "attribute3" "3")
+                                 (make-xml-attribute "attribute4" "4"))
+                         "aTTriUte5" "5"
+                         ,(make-xml-attribute "attribute6" "6"))>}))
