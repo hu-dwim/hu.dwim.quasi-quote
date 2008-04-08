0e08f13416b8ec49e41686bf0d8d2f5625ee0483
attila.lendvai <attila.lendvai@gmail.com>
2008-04-08 20:05:56 +0000

    some more duplicates

--- src/duplicates.lisp
+++ src/duplicates.lisp
@@ -8,6 +8,11 @@
 
 ;;; THE CONTENT OF THIS FILE IS COPIED OVER FROM SOME OTHER LIBRARIES TO DECREASE DEPENDENCIES
 
+(defun import-duplicate-symbols (&optional (package *package*))
+  (import
+   '(shrink-vector if-bind aif when-bind awhen prog1-bind aprog1 it)
+   package))
+
 (defun shrink-vector (str size)
   "Only callable on local vectors with fill pointer"
   #+allegro
@@ -24,7 +29,28 @@
   (setq str (subseq str 0 size))
   str)
 
-(defun import-duplicate-symbols (&optional (package *package*))
-  (import
-   '(shrink-vector)
-   package))
+(defmacro if-bind (var test &body then/else)
+  (assert (first then/else)
+          (then/else)
+          "IF-BIND missing THEN clause.")
+  (destructuring-bind (then &optional else)
+      then/else
+    `(let ((,var ,test))
+       (if ,var ,then ,else))))
+
+(defmacro aif (test then &optional else)
+  `(if-bind it ,test ,then ,else))
+
+(defmacro when-bind (var test &body body)
+  `(if-bind ,var ,test (progn ,@body)))
+
+(defmacro awhen (test &body body)
+  `(when-bind it ,test ,@body))
+
+(defmacro prog1-bind (var ret &body body)
+  `(let ((,var ,ret))
+    ,@body
+    ,var))
+
+(defmacro aprog1 (ret &body body)
+  `(prog1-bind it ,ret ,@body))
