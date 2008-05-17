4e5e62e5386c17022730a45573855c5058190788
attila.lendvai <attila.lendvai@gmail.com>
2008-05-17 15:22:36 +0000

    implement escape-as-js-string

--- src/js/escaping.lisp
+++ src/js/escaping.lisp
@@ -6,7 +6,27 @@
 
 (in-package :cl-quasi-quote-js)
 
-(def (function eo) escape-as-js (string &optional destination)
-  (declare (ignore destination))
-  ;; TODO
-  string)
+(def (function eo) escape-as-js-string (string &optional destination)
+  (declare (type string string)
+           (type (or null (array character (*))) destination))
+  (bind ((result destination))
+    (iter (for index :from 0 :below (length string))
+          (for character = (aref string index))
+          (for character-code = (char-code character))
+          (if (or (eql character-code #.(char-code #\\))
+                  (eql character-code #.(char-code #\'))
+                  (eql character-code #.(char-code #\")))
+              (progn
+                (unless result
+                  (setf result (make-array (floor (* 1.1 (length string)))
+                                           :element-type 'character
+                                           :adjustable #t
+                                           :fill-pointer index))
+                  (replace result string :end2 index))
+                (vector-push-extend #\\ result)
+                (vector-push-extend character result))
+              (when result
+                (vector-push-extend character result))))
+    (if result
+        (shrink-vector result (length result))
+        string)))
