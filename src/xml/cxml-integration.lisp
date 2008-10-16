659d86b8126b09abddf4e679cb42f62160e8ab6c
attila.lendvai <attila.lendvai@gmail.com>
2008-10-16 17:21:57 +0000

    small changes to cxml-integration.lisp

--- src/xml/cxml-integration.lisp
+++ src/xml/cxml-integration.lisp
@@ -6,45 +6,39 @@
 
 (in-package :cl-quasi-quote-xml)
 
-(define-constant +xml-namespace-uri+ "http://www.w3.org/XML/1998/namespace" :test #'string=)
-
-(unless (boundp '+whitespace-char-string+)
-    (defconstant +whitespace-char-string+
-      (coerce
-       '(#\Space #\Tab #\Linefeed #\Return #\Page)
-       'string)
-      "A string of all characters which are considered to be whitespace.
-Same as Perl's [\\s]."))
+(def (constant :test #'string=) +xml-namespace-uri+ "http://www.w3.org/XML/1998/namespace")
+(def (constant :test #'string=) +whitespace-characters+ (coerce
+                                                          '(#\Space #\Tab #\Linefeed #\Return #\Page)
+                                                          'simple-base-string)
+    "A string of all characters which are considered to be whitespace. Same as Perl's [\\s].")
 
 (defclass sax-handler (sax:default-handler)
   ())
 
 (defmethod sax:start-element ((builder sax-handler) namespace-uri local-name qname attributes)
-  (locally (declare (special *indent*))
-    (format *standard-output* (concatenate 'string "~%~" (format nil "~A" *indent*) "@T<~A") qname )
-    (incf *indent*)
-    (and (plusp (length attributes)) (format *standard-output* " ("))
-    (iter (for attribute :in attributes)
-          (if-first-time () (format *standard-output* " "))
-          (format *standard-output* "~A \"~A\"" (sax:attribute-qname attribute) (sax:attribute-value attribute)))
-    (and (plusp (length attributes)) (format *standard-output* ")"))))
-
+  (format *standard-output* "~%~A<~A" (make-string-of-spaces (* 2 *xml-indent-level*)) qname)
+  (incf *xml-indent-level*)
+  (when (plusp (length attributes))
+    (format *standard-output* " ("))
+  (iter (for attribute :in attributes)
+        (unless (first-time-p)
+          (write-string " "))
+        (format *standard-output* "~A \"~A\"" (sax:attribute-qname attribute) (sax:attribute-value attribute)))
+  (when (plusp (length attributes))
+    (format *standard-output* ")")))
 
 (defmethod sax:end-element ((builder sax-handler) namespace-uri local-name qname)
-  (locally (declare (special *indent*))
-    (format *standard-output* ">")
-    (decf *indent*)))
+  (write-string ">")
+  (decf *xml-indent-level*))
 
 (defmethod sax:characters ((builder sax-handler) data)
-  (bind ((chars (escape-as-xml (string-right-trim +whitespace-char-string+
-                                    (string-left-trim +whitespace-char-string+ data)))))
-    (and (plusp (length chars)) (format *standard-output* " \"~A\"" chars))))
+  (bind ((chars (escape-as-xml (string-trim +whitespace-characters+ data))))
+    (when (plusp (length chars))
+      (format *standard-output* " \"~A\"" chars))))
 
 (def (function e) print-quasi-quoted-xml (input)
   "Emit an xml input as quasi quoted string. Input can be anything acceptable by cxml:parse."
-  (flet ((resolver (pubid sysid)
-	 (declare (ignore pubid sysid))
-	 (flexi-streams:make-in-memory-input-stream nil)))
-    (let ((*indent* 0))
-      (declare (special *indent*))
-      (cxml:parse input (make-instance 'sax-handler) :entity-resolver #'resolver))))
+  (bind ((*xml-indent-level* 0))
+    (cxml:parse input (make-instance 'sax-handler)
+                :entity-resolver (constantly (make-in-memory-input-stream ""))
+                :validate #f)))
