8e334f19374b50abaf2b471e2d2a903d236475ce
attila.lendvai <attila.lendvai@gmail.com>
2010-04-02 13:07:08 +0000

    emacs integration: use colon for prefix separator in names to resemble CL packages

--- emacs/hu.dwim.quasi-quote.el
+++ emacs/hu.dwim.quasi-quote.el
@@ -16,12 +16,12 @@
 ;;
 ;; (define-key slime-mode-map (kbd "C-w") (lambda (n)
 ;;                                          (interactive "P")
-;;                                          (hu.dwim.quasi-quote.wrap-selection-or-sexp nil n)))
+;;                                          (hu.dwim.quasi-quote:wrap-selection-or-sexp nil n)))
 ;; (define-key slime-mode-map (kbd "C-S-w") (lambda (n)
 ;;                                            (interactive "P")
-;;                                            (hu.dwim.quasi-quote.wrap-selection-or-sexp t n)))
+;;                                            (hu.dwim.quasi-quote:wrap-selection-or-sexp t n)))
 
-(defvar hu.dwim.quasi-quote.xml-syntax-table
+(defvar hu.dwim.quasi-quote:xml-syntax-table
   (let ((table (copy-syntax-table lisp-mode-syntax-table)))
     (modify-syntax-entry ?< "(>" table)
     (modify-syntax-entry ?> ")<" table)
@@ -47,13 +47,13 @@
   "Face for the element name in the <element ()> syntax."
   :group 'hu.dwim.quasi-quote.faces)
 
-(defun hu.dwim.quasi-quote.mark-text-as-xml-paren (start end)
+(defun hu.dwim.quasi-quote:mark-text-as-xml-paren (start end)
   (add-text-properties start end
                        `(face hu.dwim.quasi-quote.xml-paren-face
-                         syntax-table ,hu.dwim.quasi-quote.xml-syntax-table)))
+                         syntax-table ,hu.dwim.quasi-quote:xml-syntax-table)))
 
-(defun hu.dwim.quasi-quote.lisp-mode-hook ()
-  (hu.dwim.quasi-quote.install-js-indentations)
+(defun hu.dwim.quasi-quote:lisp-mode-hook ()
+  (hu.dwim.quasi-quote:install-js-indentations)
   (mapcar (lambda (parens)
             (let ((open (elt parens 0))
                   (close (elt parens 1)))
@@ -77,13 +77,13 @@
    nil `(("\\(`ui\\|`xml\\|`js-inline\\|`js-xml\\|`js-piece\\|`js\\|`str\\|`\\|,\\)" 1 'hu.dwim.quasi-quote.quasi-quote-face)
          ("[ 	\n`]\\(<\\)\\(\\w+\\|,\\)"
           (0 (progn
-               (hu.dwim.quasi-quote.mark-text-as-xml-paren (match-beginning 1) (match-end 1))
+               (hu.dwim.quasi-quote:mark-text-as-xml-paren (match-beginning 1) (match-end 1))
                nil)
              prepend)
           (2 'hu.dwim.quasi-quote.xml-element-face))
          ("[^-'=/<>(]\\(>+\\)"
           (0 (progn
-               (hu.dwim.quasi-quote.mark-text-as-xml-paren (match-beginning 1) (match-end 1))
+               (hu.dwim.quasi-quote:mark-text-as-xml-paren (match-beginning 1) (match-end 1))
                (hu.dwim.quasi-quote:%mark-upcoming-xml-parens)))))
    'append)
   ;; set up some appended rules that remove it
@@ -94,19 +94,21 @@
                                        `(syntax-table nil))
                nil))))))
 
+(add-hook 'lisp-mode-hook 'hu.dwim.quasi-quote:lisp-mode-hook)
+
 ;; TODO wtf?, it seems to be not getting triggered...
 (defun hu.dwim.quasi-quote:%mark-upcoming-xml-parens ()
   ;; ok, and now let's go until the end of line and while we only see close parens
   ;; mark all >'s as an xml close paren
   (let ((index (match-end 1)))
     (while (or (member (char-after index) '(?\  ?\"))
-               (find (char-after index) hu.dwim.quasi-quote.paren-pairs :key 'second))
+               (find (char-after index) hu.dwim.quasi-quote:paren-pairs :key 'second))
       (when (equal (char-after index) ?> )
-        (hu.dwim.quasi-quote.mark-text-as-xml-paren index (1+ index)))
+        (hu.dwim.quasi-quote:mark-text-as-xml-paren index (1+ index)))
       (incf index)))
   nil)
 
-(defun hu.dwim.quasi-quote.install-js-indentations ()
+(defun hu.dwim.quasi-quote:install-js-indentations ()
   (let ((overrides '((try unwind-protect))))
     (dolist (el overrides)
       (put (first el) 'common-lisp-indent-function
@@ -114,9 +116,7 @@
                (get (second el) 'common-lisp-indent-function)
                (second el))))))
 
-(add-hook 'lisp-mode-hook 'hu.dwim.quasi-quote.lisp-mode-hook)
-
-(defvar hu.dwim.quasi-quote.paren-pairs
+(defvar hu.dwim.quasi-quote:paren-pairs
   (mapcar
    (lambda (el)
      (list (elt el 0) (elt el 1)))
@@ -125,7 +125,7 @@
      "〖〗" "〘〙" "〚〛" "（）" "＜＞" "［］" "｛｝" "｢｣"
      )))
 
-(defun hu.dwim.quasi-quote.paren-characters-for-context ()
+(defun hu.dwim.quasi-quote:paren-characters-for-context ()
   (save-excursion
     (catch 'return
       (let ((start (point))
@@ -133,7 +133,7 @@
             (direction 1)
             (reverse-parens (mapcar (lambda (el)
                                       (list (cadr el) (car el)))
-                                    hu.dwim.quasi-quote.paren-pairs)))
+                                    hu.dwim.quasi-quote:paren-pairs)))
         (while (and (< distance 50)
                     (not (eobp))
                     (not (bobp)))
@@ -142,7 +142,7 @@
               (when (eql direction 1)
                 (goto-char start)
                 (setq direction -1))
-              (let ((parens (assoc (char-after) hu.dwim.quasi-quote.paren-pairs)))
+              (let ((parens (assoc (char-after) hu.dwim.quasi-quote:paren-pairs)))
                 (unless parens
                   (setq parens (assoc (char-after) reverse-parens))
                   (when parens
@@ -154,34 +154,34 @@
               (backward-char))))
       (list ?\( ?\)))))
 
-(defun hu.dwim.quasi-quote.sexp-separator-p (char)
+(defun hu.dwim.quasi-quote:sexp-separator-p (char)
   (member char (list ?\n ?\, ?\' ?\` ?\t ?\  ?\( ?\) ?\[ ?\] ?\< ?\>)))
 
-(defun hu.dwim.quasi-quote.before-sexp-separator-p ()
+(defun hu.dwim.quasi-quote:before-sexp-separator-p ()
   (or (bolp)
-      (hu.dwim.quasi-quote.sexp-separator-p (char-after))))
+      (hu.dwim.quasi-quote:sexp-separator-p (char-after))))
 
-(defun hu.dwim.quasi-quote.after-sexp-separator-p ()
+(defun hu.dwim.quasi-quote:after-sexp-separator-p ()
   (or (eolp)
-      (hu.dwim.quasi-quote.sexp-separator-p (char-before))))
+      (hu.dwim.quasi-quote:sexp-separator-p (char-before))))
 
-(defun hu.dwim.quasi-quote.wrap-selection-or-sexp (dwim-parens &optional n)
+(defun hu.dwim.quasi-quote:wrap-selection-or-sexp (dwim-parens &optional n)
   "If selection is active, then wrap it with parens. If DWIM-PARENS is T, then chose the wrapping parens by looking around in the context."
-  (if (or (hu.dwim.quasi-quote.after-sexp-separator-p)
-          (hu.dwim.quasi-quote.before-sexp-separator-p))
+  (if (or (hu.dwim.quasi-quote:after-sexp-separator-p)
+          (hu.dwim.quasi-quote:before-sexp-separator-p))
       (if dwim-parens
-          (hu.dwim.quasi-quote.wrap-sexp n)
+          (hu.dwim.quasi-quote:wrap-sexp n)
           (paredit-wrap-sexp n))
       (save-excursion
         (backward-sexp)
         (if dwim-parens
-            (hu.dwim.quasi-quote.wrap-sexp n)
+            (hu.dwim.quasi-quote:wrap-sexp n)
             (paredit-wrap-sexp n)))))
 
-(defun hu.dwim.quasi-quote.wrap-sexp (&optional n)
+(defun hu.dwim.quasi-quote:wrap-sexp (&optional n)
   "Wrap the following S-expression in parens dwim-ishly finding out which paren characters to use. Otherwise behaves just like paredit-wrap-sexp."
   (interactive "P")
-  (let ((parens (hu.dwim.quasi-quote.paren-characters-for-context)))
+  (let ((parens (hu.dwim.quasi-quote:paren-characters-for-context)))
     (paredit-handle-sexp-errors
         (paredit-insert-pair (or n
                                  (and (not (paredit-region-active-p))
