94b7792af91051accd2a62bafe04f89e440db888
attila.lendvai <attila.lendvai@gmail.com>
2011-02-11 00:25:24 +0000

    follow util: shrink-vector was moved there

--- source/duplicates.lisp
+++ source/duplicates.lisp
@@ -17,22 +17,6 @@
   (declare (type array-index initial-length))
   (make-array initial-length :adjustable t :fill-pointer 0 :element-type element-type))
 
-(def (function io) shrink-vector (str &optional (size (length str)))
-  "Only callable on local vectors with fill pointer"
-  #+allegro
-  (excl::.primcall 'sys::shrink-svector str size)
-  #+sbcl
-  (setq str (sb-kernel:%shrink-vector str size))
-  #+cmu
-  (lisp::shrink-vector str size)
-  #+lispworks
-  (system::shrink-vector$vector str size)
-  #+scl
-  (common-lisp::shrink-vector str size)
-  #-(or allegro cmu lispworks sbcl scl)
-  (setq str (subseq str 0 size))
-  str)
-
 ;; cl-l10n
 (defun capitalize-first-letter (str)
   (if (and (> (length str) 0)
